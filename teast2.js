72689625a7dde4eb482a0a332d643dc02d58659d
pkd929 <dubeyprashant090@gmail.com>
2023-03-21 18:34:43 +0530

    test 2

--- teast2.js
+++ teast2.js
@@ -34,6 +34,26 @@ function permute(num) {
     return result;
 }
 
+
+
+const arr = [1, 2, 3];
+const findPermutations = (arr = []) => {
+   let res = []
+   const helper = (arr2) => {
+      if (arr2.length==arr.length)
+      return res.push(arr2)
+      for(let e of arr)
+      if (!arr2.includes(e))
+      helper([...arr2, e])
+   };
+   helper([])
+   return res;
+};
+console.log(findPermutations(arr));
+
+
+
+
     
     
   
